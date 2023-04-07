933593f614040d983f43a27516901f790e99bf23
Noaa Maman <noaamaman325158@gmail.com>
2023-04-07 06:27:29 +0300

    Here the tokenizer provide in api some access token

---
+++ BankAPI/AutenAuthorAPI/Controllers/AuthController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace AutenAuthorAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] Credential credential)
+        {
+            //Verify the credentials
+            if (credential.UserName == "admin" && credential.Password == "password")
+            {
+                //Creating the security context
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com"),
+                    new Claim("Admin","true"),
+                    new Claim("Manager","true"),
+                    new Claim("Department", "ConsultingBankers"),
+                    new Claim("EmploymentDate","2022-05-01")
+                };
+
+                var expiresAt = DateTime.UtcNow.AddMinutes(10);
+                return Ok(new
+                {
+                    access_token = CreateToken(claims, expiresAt),
+                    expiresAt = expiresAt,
+                });
+            }
+
+            ModelState.AddModelError("Unauthorized", "You are not authorized to access the endpoint");
+            return Unauthorized(ModelState);
+        }
+
+        private string CreateToken(IEnumerable<Claim> claims, DateTime expiresAt)
+        {
+            var secretKey = _configuration.GetValue<string>("SecretKey") ?? throw new ArgumentNullException("SecretKey is missing in configuration.");
+
+            var jwt = new JwtSecurityToken(
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: expiresAt,
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey)),
+                    SecurityAlgorithms.HmacSha256Signature));
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+    }
+
+    public class Credential
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
