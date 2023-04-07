d1f21be0333cfe759768bf0a02a44fb12e8c0864
Noaa Maman <noaamaman325158@gmail.com>
2023-04-07 19:29:58 +0300

    Succedd to merge the the razor and mvc logic - there is stil problemm with the common layout styling

--- BankAPI/WebApplication/Startup.cs
+++ BankAPI/WebApplication/Startup.cs
@@ -108,6 +108,9 @@ namespace WebApplication
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
