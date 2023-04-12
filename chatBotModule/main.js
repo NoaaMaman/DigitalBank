e4466f033985a73f4dc436ab897e15d50c512e06
Noaa Maman <noaamaman325158@gmail.com>
2023-04-12 17:21:24 +0300

    finish js chat bot

--- chatBotModule/main.js
+++ chatBotModule/main.js
@@ -15,5 +15,21 @@ function sendMessage(){
     const message = inputField.value;
     inputField.value = '';
     chatBoxBody.innerHTML += '<div class="chat-box-message">${message}</div>'
+    scrollToBottom();
 
+    fetch('http://localhost:3000/message',{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({ message })
+    }).then(response => response.json())
+    .then(data =>{
+        chatBoxBody.innerHTML += '<div class="chat-box-message">${data.message}</div>'
+        scrollToBottom();
+    });
+}
+
+function scrollToBottom(){
+    chatBoxBody.scrollTop = chatBoxBody.scrollHeight;
 }
